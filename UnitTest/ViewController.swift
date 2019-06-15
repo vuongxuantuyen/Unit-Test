67103bb13c1de222ea6e3a2d0530d12b080d6bed
Vương Xuân Tuyến <vuongxuantuyen@gmail.com>
2019-06-15 23:32:46 +0700

    Demo Unit Test

--- UnitTest/ViewController.swift
+++ UnitTest/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
 }
 
+class ComplexNumber {
+    var realValue: Float = 0
+    var imaginaryValue: Float = 0
+        
+    init(_ r: Float,_ i: Float) {
+        realValue = r
+        imaginaryValue = i
+        
+    }
+    
+    func multiply(with complexNumber: ComplexNumber) -> ComplexNumber {
+        let resultRealValue = realValue * complexNumber.realValue -
+            imaginaryValue * complexNumber.imaginaryValue
+        let resultImaginaryValue = (realValue * complexNumber.imaginaryValue) + (imaginaryValue * complexNumber.realValue)
+        return ComplexNumber(resultRealValue, resultImaginaryValue)
+    }
+}
+
+extension ComplexNumber: Equatable {
+    static func == (lhs: ComplexNumber, rhs: ComplexNumber) -> Bool {
+        return lhs.realValue == rhs.realValue && lhs.imaginaryValue == rhs.imaginaryValue
+    }
+}
--- UnitTestTests/UnitTestTests.swift
+++ UnitTestTests/UnitTestTests.swift
@@ -11,8 +11,13 @@ import XCTest
 
 class UnitTestTests: XCTestCase {
 
+    private var testNumber: ComplexNumber!
+    private var multiplyNumber: ComplexNumber!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testNumber = ComplexNumber(0, 0)
+        
+        multiplyNumber = ComplexNumber(0, 0)
     }
 
     override func tearDown() {
@@ -22,6 +27,34 @@ class UnitTestTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Check case 1 : (0,0)*(0,0)
+        XCTAssertEqual(result(), ComplexNumber(0, 0))
+        // Check case multiply with 1
+        testNumber = random()
+        multiplyNumber = ComplexNumber (1, 0)
+        XCTAssertEqual(result(), testNumber)
+        // Check case multiply with 0
+        multiplyNumber = ComplexNumber(0, 0)
+        XCTAssertEqual(result(), ComplexNumber(0, 0))
+        // a*b = b*a
+        multiplyNumber = random()
+        XCTAssertEqual(result(), multiplyNumber.multiply(with: testNumber))
+        // Other case
+        testNumber = ComplexNumber(1, 2)
+        multiplyNumber = ComplexNumber(3, 1)
+        XCTAssertEqual(result(), ComplexNumber(1, 7))
+        // Other case
+        testNumber = ComplexNumber(-1, -2)
+        multiplyNumber = ComplexNumber(4, 5)
+        XCTAssertEqual(result(), ComplexNumber(6, -13))
+    }
+    
+    func result() -> ComplexNumber {
+        return testNumber.multiply(with: multiplyNumber)
+    }
+    
+    func random() -> ComplexNumber {
+        return ComplexNumber(Float.random(in: 0...100), Float.random(in: 0...100))
     }
 
     func testPerformanceExample() {
